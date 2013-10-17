5f44e11e82bfd49a7249c8eb7e983b4c799f0e2d
Apprentice <apprentice@dbc-23.local>
2013-10-17 16:19:34 -0700

    update spot migration for reflect new location variables

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017185357) do
+ActiveRecord::Schema.define(version: 20131017230745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "spots", force: true do |t|
     t.integer  "user_id"
-    t.string   "location"
     t.integer  "price"
     t.string   "image"
     t.text     "description"
     t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "street"
+    t.string   "zip_code"
   end
 
   create_table "users", force: true do |t|
