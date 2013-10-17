38e997861d55098de4db4c48e9304d279fe33c2b
Apprentice <apprentice@dbc-23.local>
2013-10-17 15:49:43 -0700

    spot controller tests

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -1,6 +1,39 @@
 require 'spec_helper'
 
 describe SpotsController do
+  describe "GET signup" do
+    it "shows a new user form" do
+    end
+  end
 
+  describe "POST signup" do
+    let(:new_spot) { FactoryGirl.build(:spot) }
+    it "creates new post when given valid params" do
+      post :create, :user => {username: new_user.username, password: new_user.password, email: new_user.email}
+      expect(assigns(:user).username).to eq(new_user.username)
+      expect(assigns(:user).password).to eq(new_user.password)
+      expect(assigns(:user).email).to eq(new_user.email)
+    end
+    it "renders correct page for new spot" do
+      expect(post :create, :user => {username: new_user.username, password: new_user.password, email: new_user.email}).to render_template :index
+    end
+    it "does not create a new spot with invalid params" do
+      post :create, :user => {username: new_user.username, password: nil, email: new_user.email}
+      expect(assigns(:user).username).to eq(nil)
+      expect(assigns(:user).password).to eq(nil)
+      expect(assigns(:user).email).to eq(nil)
+    end
+    it "renders correct page for invalid spot" do
+      expect(post :create, :user => {username: new_user.username, password: nil, email: new_user.email}).to render_template :index
+    end
+  end
 
+  describe "GET show" do
+  end
+
+  describe "GET index" do
+  end
+
+  describe "POSt destroy" do
+  end
 end
