87380d847336c168d915f1e7f66c6edefcaac946
Apprentice <apprentice@dbc-23.local>
2013-10-17 16:21:39 -0700

    update spot model testing to reflect changes made via migration to variables

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe Spot do
   it { should belong_to(:user) }
-  it { should validate_presence_of(:location) }
+  it { should validate_presence_of(:zip_code) }
+  it { should validate_presence_of(:street) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:type) }
   it { should validate_presence_of(:user) }
