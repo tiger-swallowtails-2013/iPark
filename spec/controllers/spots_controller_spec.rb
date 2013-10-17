6bd717666ba482012406a30274289283756b99ba
Apprentice <apprentice@dbc-23.local>
2013-10-17 15:52:37 -0700

    new spot controller updates for tests

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -2,29 +2,30 @@ require 'spec_helper'
 
 describe SpotsController do
   describe "GET signup" do
-    it "shows a new user form" do
+    it "shows a new spot form" do
+      expect(get :new).to render_template :new
     end
   end
 
   describe "POST signup" do
     let(:new_spot) { FactoryGirl.build(:spot) }
-    it "creates new post when given valid params" do
-      post :create, :user => {username: new_user.username, password: new_user.password, email: new_user.email}
-      expect(assigns(:user).username).to eq(new_user.username)
-      expect(assigns(:user).password).to eq(new_user.password)
-      expect(assigns(:user).email).to eq(new_user.email)
+    it "creates new spot when given valid params" do
+      post :create, :spot => {street: new_spot.street, zipcode: new_spot.zipcode, price: new_spot.price, description: new_spot.description, type: new_spot.type}
+      expect(assigns(:spot).street).to eq(new_spot.street)
+      expect(assigns(:spot).zipcode).to eq(new_spot.zipcode)
+      expect(assigns(:spot).email).to eq(new_spot.email)
     end
     it "renders correct page for new spot" do
-      expect(post :create, :user => {username: new_user.username, password: new_user.password, email: new_user.email}).to render_template :index
+      expect(post :create, :spot => {spotname: new_spot.spotname, password: new_spot.password, email: new_spot.email}).to render_template :index
     end
     it "does not create a new spot with invalid params" do
-      post :create, :user => {username: new_user.username, password: nil, email: new_user.email}
-      expect(assigns(:user).username).to eq(nil)
-      expect(assigns(:user).password).to eq(nil)
-      expect(assigns(:user).email).to eq(nil)
+      post :create, :spot => {spotname: new_spot.spotname, password: nil, email: new_spot.email}
+      expect(assigns(:spot).spotname).to eq(nil)
+      expect(assigns(:spot).password).to eq(nil)
+      expect(assigns(:spot).email).to eq(nil)
     end
     it "renders correct page for invalid spot" do
-      expect(post :create, :user => {username: new_user.username, password: nil, email: new_user.email}).to render_template :index
+      expect(post :create, :spot => {username: new_spot.username, password: nil, email: new_spot.email}).to render_template :index
     end
   end
 
