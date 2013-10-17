56af8e317b888d2f215452224b8dcf9df44cc8fd
Apprentice <apprentice@dbc-23.local>
2013-10-17 15:29:28 -0700

    add failing users controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe UsersController do
+  describe "GET signup" do
+    it "shows a new user form" do
+      expect(get :new).to render_template :new
+    end
+  end
 
+  describe "POST signup" do
+    let(:new_user) { FactoryGirl.build(:user) }
+    it "creates new user when given valid params" do
+      post :create, :user => {username: new_user.username, password: new_user.password, email: new_user.email}
+      expect(assigns(:user).username).to eq(new_user.username)
+      expect(assigns(:user).password).to eq(new_user.password)
+      expect(assigns(:user).email).to eq(new_user.email)
+    end
+    it "renders correct page for new user" do
+      expect(post :create, :user => {username: new_user.username, password: new_user.password, email: new_user.email}).to render_template :index
+    end
+    it "does not create a new user with invalid params" do
+      post :create, :user => {username: new_user.username, password: nil, email: new_user.email}
+      expect(assigns(:user).username).to eq(nil)
+      expect(assigns(:user).password).to eq(nil)
+      expect(assigns(:user).email).to eq(nil)
+    end
+    it "renders correct page for invalid user" do
+      expect(post :create, :user => {username: new_user.username, password: nil, email: new_user.email}).to render_template :index
+    end
+  end
 end
+
+
+
+  describe "POST signup" do
+    let(:new_user) { FactoryGirl.build(:user) }
+    it "creates a new user with valid info" do
+    end
+  end
