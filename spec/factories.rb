413e28fbcf447d8326633072ac9e24a870460f09
Apprentice <apprentice@dbc-23.local>
2013-10-17 16:19:50 -0700

    update factory girl for spot to reflect changes in model

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,9 +9,9 @@ FactoryGirl.define do
 
   factory :spot do
     street { Faker::Address.street_name }
-    zipcode { Faker::Address.zip_code }
+    zip_code { Faker::Address.zip_code }
     price { rand(1..10) }
     description { Faker::Company.catch_phrase }
-    type  rand['garage', 'street', 'yard', 'driveway']
+    type { ['garage', 'street', 'yard', 'driveway'].sample }
   end
 end
