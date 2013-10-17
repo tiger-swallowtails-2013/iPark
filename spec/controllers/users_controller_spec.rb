695a04372927d4da0d787a37b3a2893ec2a055f9
Apprentice <apprentice@dbc-23.local>
2013-10-17 16:06:14 -0700

    updated user and spot controller tests to reflect count change

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,19 +10,13 @@ describe UsersController do
   describe "POST signup" do
     let(:new_user) { FactoryGirl.build(:user) }
     it "creates new user when given valid params" do
-      post :create, :user => {username: new_user.username, password: new_user.password, email: new_user.email}
-      expect(assigns(:user).username).to eq(new_user.username)
-      expect(assigns(:user).password).to eq(new_user.password)
-      expect(assigns(:user).email).to eq(new_user.email)
+      expect(post :create, :user => {username: new_user.username, password: new_user.password, email: new_user.email}).to change(User, :count).by(1)
     end
     it "renders correct page for new user" do
       expect(post :create, :user => {username: new_user.username, password: new_user.password, email: new_user.email}).to render_template :index
     end
     it "does not create a new user with invalid params" do
-      post :create, :user => {username: new_user.username, password: nil, email: new_user.email}
-      expect(assigns(:user).username).to eq(nil)
-      expect(assigns(:user).password).to eq(nil)
-      expect(assigns(:user).email).to eq(nil)
+      expect(post :create, :user => {username: new_user.username, password: nil, email: new_user.email}).not_to change(User, :count)
     end
     it "renders correct page for invalid user" do
       expect(post :create, :user => {username: new_user.username, password: nil, email: new_user.email}).to render_template :index
