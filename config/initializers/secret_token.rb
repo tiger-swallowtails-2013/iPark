56911039f4153f0c97094fd27400ebd34ef63b83
Apprentice <apprentice@dbc-23.local>
2013-10-17 11:51:36 -0700

    add rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IPark::Application.config.secret_key_base = 'cdfdaf8042f70022726ebff0990f90450accf2699b5b3dea5fa94671231e7823a5bdabe8a4aaf1ad85f197734e3e6649ca35a95341466c243f70ee1b4bbb5aa4'
