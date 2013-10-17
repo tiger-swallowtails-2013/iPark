21ab0e08a55540e7f9b4ef87ef7fdbb4b36a3dca
Apprentice <apprentice@dbc-23.local>
2013-10-17 16:18:42 -0700

    chnaged spotcontroller spec to fix bugs related to factory girl

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -10,16 +10,16 @@ describe SpotsController do
   describe "POST signup" do
     let(:new_spot) { FactoryGirl.build(:spot) }
     it "creates new spot when given valid params" do
-      expect(post :create, :spot => {street: new_spot.street, zipcode: new_spot.zipcode, price: new_spot.price, description: new_spot.description, type: new_spot.type}).to change(Spot, :count).by(1)
+      expect(post :create, :spot => {street: new_spot.street, zip_code: new_spot.zip_code, price: new_spot.price, description: new_spot.description, type: new_spot.type}).to change(Spot, :count).by(1)
     end
     it "renders correct page for new spot" do
-      expect(post :create, :spot => {street: new_spot.street, zipcode: new_spot.zipcode, price: new_spot.price, description: new_spot.description, type: new_spot.type}).to render_template :index
+      expect(post :create, :spot => {street: new_spot.street, zip_code: new_spot.zip_code, price: new_spot.price, description: new_spot.description, type: new_spot.type}).to render_template :index
     end
     it "does not create a new spot with invalid params" do
-      expect(post :create, :spot => {street: new_spot.street, zipcode: new_spot.zipcode, price: new_spot.price, description: new_spot.description, type: new_spot.type}).not_to change(Spot, :count)
+      expect(post :create, :spot => {street: new_spot.street, zip_code: new_spot.zip_code, price: new_spot.price, description: new_spot.description, type: new_spot.type}).not_to change(Spot, :count)
     end
     it "renders correct page for invalid spot" do
-      expect(post :create, :spot => {street: new_spot.street, zipcode: new_spot.zipcode, price: new_spot.price, description: new_spot.description, type: new_spot.type}).to render_template :index
+      expect(post :create, :spot => {street: new_spot.street, zip_code: new_spot.zip_code, price: new_spot.price, description: new_spot.description, type: new_spot.type}).to render_template :index
     end
   end
 
@@ -37,14 +37,13 @@ describe SpotsController do
   end
 
   describe "POST destroy" do
-    before(:each) do
-      let(:destroy_spot) {FactoryGirl.create(:spot)}
-    end
+    let(:destroy_spot) {FactoryGirl.create(:spot)}
+    let(:destroy_spot2) {FactoryGirl.create(:spot)}
     it "destroys spot" do
-      expect(post :destroy, id: get_spot.id).to change(Spot, :count).by(1)
+      expect(post :destroy, id: destroy_spot.id).to change(Spot, :count).by(1)
     end
     it "renders correct page" do
-      expect(post :destroy, id: get_spot.id).to render_template :index
+      expect(post :destroy, id: destroy_spot2.id).to render_template :index
     end
   end
 end
