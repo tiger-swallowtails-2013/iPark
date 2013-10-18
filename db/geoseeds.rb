8407077a79c2b13ffb5edd0ff8f1e160832dc728
nathanallen <thenathanator@gmail.com>
2013-10-18 15:25:20 -0700

    fix seeds, factor out street_names and Neighborhoods to own file
    Faker user_name and email in wrong fields
    Refactored conditional rescues

---
+++ db/geoseeds.rb
@@ -0,0 +1,31 @@
+STREET_NAMES = [
+  "Alemany Boulevard", "Alvarado Street", "Ambrose Bierce Alley", "Anza Street", "Arguello Boulevard", "Ashbury Street",
+  "Baker Street", "Balboa Street", "Bartlett Street", "Beale Street", "Bernal Avenue", "Bluxome Street", "Brannan Street",
+  "Brenham Place", "Broderick Street", "Bryant Street", "Buchanan Street", "Burnett Avenue", "Bush Street", "Cabrillo Street",
+  "California Street", "Castro Street", "Cesar Chavez Street", "Clark Street", "Cole Street", "Coleman Street",
+  "Colin P Kelly Junior Street", "Davidson Street", "Davis Street", "De Haro Street", "Dirk Dirksen Place", "Divisadero Street",
+  "Dolores Street", "Duncan Street", "Elizabeth Street", "Fillmore Street", "Folsom Street", "Franklin Street", "Fremont Street",
+  "Fulton Street", "Funston Street", "Geary Boulevard", "Gough Street", "Grant Street", "Guerrero Street", "Haight Street",
+  "Harrison Street", "Hayes Street", "Howard Street", "Hyde Street", "Ingalls Street", "Irving Street", "Jack Kerouac Alley",
+  "Jack Micheline Alley", "Jackson Street", "Jefferson Street", "Jerrold Avenue", "Jersey Street", "John F. Shelley Drive",
+  "John Muir Drive", "Jones Street", "Joost Avenue", "Jose Sarria Court", "Juan Bautista Circle", "Judah Street",
+  "Junipero Serra Boulevard", "Justin Herman Plaza", "Kearny Street", "Keyes Avenue", "Kezar Drive", "Kirkham Street",
+  "Laguna Street", "Lane Street", "Lapham Way", "Lapu Lapu Street", "Larkin Street", "Laussat Street", "Lawton Street",
+  "Leavenworth Street", "Le Conte Avenue", "Lech Walesa Street", "Leese Street", "Leidesdorff Street", "Lendrum Street",
+  "Liggett Avenue", "Linares Avenue", "Lincoln Boulevard", "Lincoln Court", "Lincoln Way", "Lombard Street", "Mason Street",
+  "McAllister Street", "Montgomery Street", "Moraga Street", "Noe Street", "Noriega Street", "Octavia Street", "O'Farrell Street",
+  "Ortega Street", "O'Shaughnessy Boulevard", "Otis Street", "Pacheco Street", "Palou Avenue", "Peter Yorke Way", "Phelan Avenue",
+  "Pierce Street", "Polk Street", "Portola Avenue", "Potrero Avenue", "Rivera Street", "Rolph Street", "Sanchez Street",
+  "Sansome Street", "Scott Street", "Selby Street", "Shafter Avenue", "Sheridan Street", "Shotwell Street", "Shrader Street.",
+  "Sloat Boulevard", "Spear Street", "Stanyan Street", "Steiner Street", "Steuart Street", "Stockton Street", "Sutter Street",
+  "Taraval Street", "Taylor Street", "Terry A Francois Boulevard", "Thomas Avenue", "Tonquin Street", "Townsend Street",
+  "Treat Street", "Turk Street", "Ulloa Street", "Valencia Street", "Vallejo Street", "Van Ness Avenue", "Via Ferlinghetti",
+  "Vicente Street", "Waller Street", "Washington Street", "Wawona Street", "Webb Street", "Yorba Street", "Zarick Street"
+]
+
+NEIGHBORHOODS = [
+  "Bernal Heights", "Castro", "Chinatown", "Cole Valley", "Deco Ghetto", "Fisherman's Wharf", "Golden Gate Park", "Haight",
+  "Hayes Valley", "Inner Richmond", "Inner Sunset", "Marina", "Mission", "Mission to Potrero", "Dolores", "Valencia Corridor",
+  "24th Street", "Nob Hill", "Noe Valley", "North Beach", "Outer Richmond", "Outer Sunset", "Pacific Heights", "Potrero Hill",
+  "Russian Hill", "SoMa", "Tenderloin", "Union Square", "Western Addition"
+]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,27 @@
-STREET_NAMES = [
-  "Alemany Boulevard", "Alvarado St", "Ambrose Bierce Alley", "Anza St", "Arguello Boulevard", "Ashbury St",
-  "Baker St", "Balboa St", "Bartlett St", "Beale St", "Bernal Avenue", "Bluxome St", "Brannan St",
-  "Brenham Place", "Broderick Street", "Bryant Street", "Buchanan Street", "Burnett Avenue", "Bush Street", "Cabrillo Street",
-  "California Street", "Castro Street", "Cesar Chavez Street", "Clark Street", "Cole Street", "Coleman Street",
-  "Colin P Kelly Junior Street", "Davidson Street", "Davis Street", "De Haro Street", "Dirk Dirksen Place", "Divisadero Street",
-  "Dolores Street", "Duncan Street", "Elizabeth Street", "Fillmore Street", "Folsom Street", "Franklin Street", "Fremont Street",
-  "Fulton Street", "Funston Street", "Geary Boulevard", "Gough Street", "Grant Street", "Guerrero Street", "Haight Street",
-  "Harrison Street", "Hayes Street", "Howard Street", "Hyde Street", "Ingalls Street", "Irving Street", "Jack Kerouac Alley",
-  "Jack Micheline Alley", "Jackson Street", "Jefferson Street", "Jerrold Avenue", "Jersey Street", "John F. Shelley Dr.",
-  "John Muir Drive", "Jones Street", "Joost Avenue", "Jose Sarria Court", "Juan Bautista Circle", "Judah Street",
-  "Junipero Serra Boulevard", "Justin Herman Plaza", "Kearny Street", "Keyes Avenue", "Kezar Drive", "Kirkham Street",
-  "Laguna Street", "Lane St.", "Lapham Way", "Lapu Lapu St.", "Larkin St.", "Laussat St.", "Lawton St.",
-  "Leavenworth St.", "Le Conte Avenue", "Lech Walesa St.", "Leese Street", "Leidesdorff Street", "Lendrum Street",
-  "Liggett Avenue", "Linares Avenue", "Lincoln Boulevard", "Lincoln Court", "Lincoln Way", "Lombard Street", "Mason Street",
-  "McAllister Street", "Montgomery Street", "Moraga Street", "Noe Street", "Noriega Street", "Octavia Street", "O'Farrell Street",
-  "Ortega Street", "O'Shaughnessy Blvd", "Otis Street", "Pacheco Street", "Palou Avenue", "Peter Yorke Way", "Phelan Avenue",
-  "Pierce Street", "Polk Street", "Portola Avenue", "Potrero Ave", "Rivera Street", "Rolph Street", "Sanchez Street",
-  "Sansome Street", "Scott St", "Selby St", "Shafter Avenue", "Sheridan St", "Shotwell St", "Shrader St.",
-  "Sloat Boulevard", "Spear St", "Stanyan St", "Steiner St", "Steuart St", "Stockton Street", "Sutter Street",
-  "Taraval Street", "Taylor Street", "Terry A Francois Boulevard", "Thomas Avenue", "Tonquin Street", "Townsend Street",
-  "Treat St", "Turk Street", "Ulloa Street", "Valencia Street", "Vallejo Street", "Van Ness Avenue", "Via Ferlinghetti",
-  "Vicente Street", "Waller Street", "Washington Street", "Wawona Street", "Webb Street", "Yorba Street", "Zarick Street"
-]
+# Seeds development database with fake data to assist design and development.
+# Creates fake users and assigns parking spots to users
 
-NEIGHBORHOODS = [
-  "Bernal Heights", "Castro", "Chinatown", "Cole Valley", "Deco Ghetto", "Fisherman's Wharf", "Golden Gate Park", "Haight",
-  "Hayes Valley", "Inner Richmond", "Inner Sunset", "Marina", "Mission", "Mission to Potrero", "Dolores", "Valencia Corridor",
-  "24th Street", "Nob Hill", "Noe Valley", "North Beach", "Outer Richmond", "Outer Sunset", "Pacific Heights", "Potrero Hill",
-  "Russian Hill", "SoMa", "Tenderloin", "Union Square", "Western Addition"
-]
+FAKE_USERS_COUNT = 100
+FAKE_SPOTS_COUNT = 100
+
+require_relative "./geoseeds"
 
 SPOT_TYPES = [
   "parking spot", "space", "spot", "yard", "driveway", "street parking", "parking spot", "double spot", "garage",
   "covered parking place", "parking lot"
 ]
 
-NONCE = [
+STATUS = [
   "available", "ready for your car", "for rent", "from 9-5", "available weekdays", "available on weekends", "perfect for your winnebago",
   "close to AT&T Park", "near Giants Stadium", "motorcycle friendly", "going fast", "in quiet neighborhood", "for commuter", "", "", "", ""
 ]
-
-ZIP_CODES = []
 
 def generate_fake_spot_data
   my_spot = SPOT_TYPES.sample
   my_street = STREET_NAMES.sample
   my_hood = NEIGHBORHOODS.sample
-  my_chatter = NONCE.sample
-  my_description = [my_hood, my_street].sample + " " + my_spot + " " + my_chatter
+  my_status = STATUS.sample
+  my_description = [my_hood, my_street].sample + " " + my_spot + " " + my_status
   return {
       location_type: my_spot,
       description: my_description,
@@ -61,16 +35,15 @@ end
 puts "----SEEDING DATABASE-----"
 puts "\n----Creating Users-----"
 
-100.times do |create_fake_users|
+FAKE_USERS_COUNT.times do
   print "."
-  begin
-    User.create!(
-         email: Faker::Internet.user_name,
-      username: Faker::Internet.email,
-      password: "foobar",
-      password_confirmation: "foobar"
-    )
-  rescue
+  user = User.new(
+       email: Faker::Internet.email,
+    username: Faker::Internet.user_name,
+    password: "foobar",
+    password_confirmation: "foobar"
+  )
+  unless user.save
     print "x"
   end
 
@@ -80,12 +53,11 @@ puts "\n----Creating & Assigning Spots-----"
 
 VALID_USER_IDS = User.pluck(:id)
 
-80.times do |create_and_assign_spots|
+FAKE_SPOTS.times do
   print "."
-  begin
-    new_spot = Spot.create!(generate_fake_spot_data)
-  rescue
-   print "x"
+  spot = Spot.new(generate_fake_spot_data)
+  unless spot.save
+    print "x"
   end
 end
 
