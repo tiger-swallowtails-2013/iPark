1337cb310622d3626dbd97cdcc1a1fc0edcdb916
nathanallen <thenathanator@gmail.com>
2013-10-18 11:14:11 -0700

    update street names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
 STREET_NAMES = [
-  "Alemany Boulevard", "Alvarado Street", "Ambrose Bierce Alley", "Anza Street", "Arguello Boulevard", "Ashbury Street",
-  "Baker Street", "Balboa Street", "Bartlett Street", "Beale Street", "Bernal Avenue", "Bluxome Street", "Brannan Street",
+  "Alemany Boulevard", "Alvarado St", "Ambrose Bierce Alley", "Anza St", "Arguello Boulevard", "Ashbury St",
+  "Baker St", "Balboa St", "Bartlett St", "Beale St", "Bernal Avenue", "Bluxome St", "Brannan St",
   "Brenham Place", "Broderick Street", "Bryant Street", "Buchanan Street", "Burnett Avenue", "Bush Street", "Cabrillo Street",
   "California Street", "Castro Street", "Cesar Chavez Street", "Clark Street", "Cole Street", "Coleman Street",
   "Colin P Kelly Junior Street", "Davidson Street", "Davis Street", "De Haro Street", "Dirk Dirksen Place", "Divisadero Street",
   "Dolores Street", "Duncan Street", "Elizabeth Street", "Fillmore Street", "Folsom Street", "Franklin Street", "Fremont Street",
   "Fulton Street", "Funston Street", "Geary Boulevard", "Gough Street", "Grant Street", "Guerrero Street", "Haight Street",
   "Harrison Street", "Hayes Street", "Howard Street", "Hyde Street", "Ingalls Street", "Irving Street", "Jack Kerouac Alley",
-  "Jack Micheline Alley", "Jackson Street", "Jefferson Street", "Jerrold Avenue", "Jersey Street", "John F. Shelley Drive",
+  "Jack Micheline Alley", "Jackson Street", "Jefferson Street", "Jerrold Avenue", "Jersey Street", "John F. Shelley Dr.",
   "John Muir Drive", "Jones Street", "Joost Avenue", "Jose Sarria Court", "Juan Bautista Circle", "Judah Street",
   "Junipero Serra Boulevard", "Justin Herman Plaza", "Kearny Street", "Keyes Avenue", "Kezar Drive", "Kirkham Street",
-  "Laguna Street", "Lane Street", "Lapham Way", "Lapu Lapu Street", "Larkin Street", "Laussat Street", "Lawton Street",
-  "Leavenworth Street", "Le Conte Avenue", "Lech Walesa Street", "Leese Street", "Leidesdorff Street", "Lendrum Street",
+  "Laguna Street", "Lane St.", "Lapham Way", "Lapu Lapu St.", "Larkin St.", "Laussat St.", "Lawton St.",
+  "Leavenworth St.", "Le Conte Avenue", "Lech Walesa St.", "Leese Street", "Leidesdorff Street", "Lendrum Street",
   "Liggett Avenue", "Linares Avenue", "Lincoln Boulevard", "Lincoln Court", "Lincoln Way", "Lombard Street", "Mason Street",
   "McAllister Street", "Montgomery Street", "Moraga Street", "Noe Street", "Noriega Street", "Octavia Street", "O'Farrell Street",
-  "Ortega Street", "O'Shaughnessy Boulevard", "Otis Street", "Pacheco Street", "Palou Avenue", "Peter Yorke Way", "Phelan Avenue",
-  "Pierce Street", "Polk Street", "Portola Avenue", "Potrero Avenue", "Rivera Street", "Rolph Street", "Sanchez Street",
-  "Sansome Street", "Scott Street", "Selby Street", "Shafter Avenue", "Sheridan Street", "Shotwell Street", "Shrader Street.",
-  "Sloat Boulevard", "Spear Street", "Stanyan Street", "Steiner Street", "Steuart Street", "Stockton Street", "Sutter Street",
+  "Ortega Street", "O'Shaughnessy Blvd", "Otis Street", "Pacheco Street", "Palou Avenue", "Peter Yorke Way", "Phelan Avenue",
+  "Pierce Street", "Polk Street", "Portola Avenue", "Potrero Ave", "Rivera Street", "Rolph Street", "Sanchez Street",
+  "Sansome Street", "Scott St", "Selby St", "Shafter Avenue", "Sheridan St", "Shotwell St", "Shrader St.",
+  "Sloat Boulevard", "Spear St", "Stanyan St", "Steiner St", "Steuart St", "Stockton Street", "Sutter Street",
   "Taraval Street", "Taylor Street", "Terry A Francois Boulevard", "Thomas Avenue", "Tonquin Street", "Townsend Street",
-  "Treat Street", "Turk Street", "Ulloa Street", "Valencia Street", "Vallejo Street", "Van Ness Avenue", "Via Ferlinghetti",
+  "Treat St", "Turk Street", "Ulloa Street", "Valencia Street", "Vallejo Street", "Van Ness Avenue", "Via Ferlinghetti",
   "Vicente Street", "Waller Street", "Washington Street", "Wawona Street", "Webb Street", "Yorba Street", "Zarick Street"
 ]
 
@@ -42,7 +42,7 @@ NONCE = [
 
 ZIP_CODES = []
 
-def fake_spot_description
+def generate_fake_spot_data
   my_spot = SPOT_TYPES.sample
   my_street = STREET_NAMES.sample
   my_hood = NEIGHBORHOODS.sample
