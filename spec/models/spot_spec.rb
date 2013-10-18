b1d5d34af7ef5b35412d8d5011c9cbe0c6915148
Apprentice <apprentice@dbc-23.local>
2013-10-17 20:06:31 -0700

    all passing tests, final change related to model tests bugs due to changes in model

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -5,5 +5,5 @@ describe Spot do
   it { should validate_presence_of(:zip_code) }
   it { should validate_presence_of(:street) }
   it { should validate_presence_of(:price) }
-  it { should validate_presence_of(:type) }
+  it { should validate_presence_of(:location_type) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe User do
   it { should have_many(:spots) }
-  it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
