4aedae51cd9b8882ff91981c37de890d005cd15f
Apprentice <apprentice@dbc-23.local>
2013-10-17 19:51:51 -0700

    updated user controller tests that were broken

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,10 +17,15 @@ describe UsersController do
       expect(post :create, :user => {username: new_user.username, password: new_user.password, email: new_user.email, password_confirmation: new_user.password_confirmation}).to redirect_to spots_path
     end
     it "does not create a new user with invalid params" do
-      expect(post :create, :user => {username: new_user.username, password: nil, email: new_user.email}).to_not change(User, :count)
+      create_user = post :create, :user => {username: new_user.username, password: nil, email: new_user.email}
+      expect(User.count).to be(0)
     end
     it "renders correct page for invalid user" do
       expect(post :create, :user => {username: new_user.username, password: nil, email: new_user.email}).to render_template :new
     end
+  end
+
+  after(:each) do
+    User.destroy_all
   end
 end
