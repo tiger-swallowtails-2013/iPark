969212ad2102a3567e17acae7f4ce3dec2325bcc
nathanallen <thenathanator@gmail.com>
2013-10-18 11:50:53 -0700

    refactor login feature test to use login helper

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -3,12 +3,7 @@ require 'spec_helper'
 feature "user can log in" do
   let!(:current_user) {FactoryGirl.create(:user)}
   scenario "when user fills out form to log in correctly should redirect to the spot index page" do
-    visit new_session_path
-    fill_in 'sessions[username]', with:
-    current_user.username
-    fill_in 'sessions[password]', with:
-    current_user.password_confirmation
-    click_button "Sign In"
+    login(current_user)
     expect(page) == spots_path
   end
 end
