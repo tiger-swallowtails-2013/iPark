5fb428841c0e4c454457683a5c2f91bd5c329df9
Apprentice <apprentice@dbc-23.local>
2013-10-17 19:52:09 -0700

    updated SpotsController to meet needs of tests

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,2 +1,23 @@
 class SpotsController < ApplicationController
+
+  def new
+    @spot = Spot.new
+  end
+
+  def create
+    @spot = Spot.new(params.require(:spot).permit(:street, :zip_code, :price, :description, :location_type))
+    if @spot.save
+      render :index
+    else
+      redirect_to :new
+    end
+  end
+
+  def show
+    p 'got here'
+    @spot = Spot.find(params[:id])
+  end
+
+  def index
+  end
 end
