1d46a0c5a6f58bf9ec11847a8e3ee94775266e0c
morganwildermuth <morgan.wildermuth@gmail.com>
2013-10-17 21:59:47 -0700

    fixed typo related to feature test on user log in

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-feature 'user can log in' do
+feature "user can log in" do
   let!(:current_user) {FactoryGirl.create(:user)}
   scenario "when user fills out form to log in correctly should redirect to the spot index page" do
     visit new_session_path
     fill_in 'sessions[username]', with:
     current_user.username
-    fill_in 'sessions[passsword]', with:
+    fill_in 'sessions[password]', with:
     current_user.password_confirmation
     click_button "Sign In"
     expect(page) == spots_path
