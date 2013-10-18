41a5b6cd0321fafb20e6294eb6f707d98e32399b
nathanallen <thenathanator@gmail.com>
2013-10-18 16:56:34 -0700

    refactor seed file to use geocoder, more accurate spot descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
 # Seeds development database with fake data to assist design and development.
 # Creates fake users and assigns parking spots to users
-
-FAKE_USERS_COUNT = 100
-FAKE_SPOTS_COUNT = 100
+# Uses fake street addresses to query google for geolocation data
 
 require_relative "./geoseeds"
+
+FAKE_USERS_COUNT = 1
+FAKE_SPOTS_COUNT = 2
+CREATE_TEST_USERS = false
+GOOGLE_QUERY_SLEEPTIME = 0.25 # necessary to avoid exceeding google's per-second limit
 
 SPOT_TYPES = [
   "parking spot", "space", "spot", "yard", "driveway", "street parking", "parking spot", "double spot", "garage",
@@ -26,63 +29,78 @@ def generate_fake_spot_data
       location_type: my_spot,
       description: my_description,
       street: rand(1..3000).to_s + " " + my_street,
-      zip_code: 94102 + rand(88),
+      zip_code: 94102 + rand(88), # field is reassigned by geocoder in Spot model
       price: rand(1..30),
       user_id: VALID_USER_IDS.sample
     }
+end
+
+def generate_more_accurate_spot_description(zip_code)
+  my_hood = lookup_neighborhood_of_zipcode(zip_code)
+  my_spot = SPOT_TYPES.sample
+  my_status = STATUS.sample
+  my_description = my_hood + " " + my_spot + " " + my_status
+end
+
+def lookup_neighborhood_of_zipcode(zip_code)
+  if (94102..94134)===zip_code
+    ZIP_CODE_NEIGHBORHOOD_LOOKUP[zip_code.to_s].sample
+  else
+    NEIGHBORHOODS.sample + "*"
+  end
 end
 
 puts "----SEEDING DATABASE-----"
 puts "\n----Creating Users-----"
 
 FAKE_USERS_COUNT.times do
-  print "."
   user = User.new(
        email: Faker::Internet.email,
     username: Faker::Internet.user_name,
     password: "foobar",
     password_confirmation: "foobar"
   )
-  unless user.save
-    print "x"
-  end
-
+  user.save ? (print ".") : (print "x")
 end
 
 puts "\n----Creating & Assigning Spots-----"
+puts "This will take #{FAKE_SPOTS_COUNT*GOOGLE_QUERY_SLEEPTIME} seconds"
 
 VALID_USER_IDS = User.pluck(:id)
 
-FAKE_SPOTS.times do
-  print "."
+FAKE_SPOTS_COUNT.times do
   spot = Spot.new(generate_fake_spot_data)
-  unless spot.save
-    print "x"
+  if spot.save
+    spot.description = generate_more_accurate_spot_description(spot.zip_code)
+    print "."
+  else
+    print "x")
+  end
+  sleep GOOGLE_QUERY_SLEEPTIME
+end
+
+if CREATE_TEST_USERS
+  puts "\n----Creating Test Accounts-----"
+  1.times do
+    User.create!(
+         email: 'test@test.com',
+      username: 'test',
+      password: 'test',
+      password_confirmation: 'test'
+    )
+    User.create!(
+         email: 'nishant@test.com',
+      username: 'nishant',
+      password: 'nishant',
+      password_confirmation: 'nishant'
+    )
+    User.create!(
+         email: 'morgan@test.com',
+      username: 'morgan',
+      password: 'morgan',
+      password_confirmation: 'morgan'
+    )
   end
 end
-
-puts "\n----Creating Test Accounts-----"
-
-1.times do |make_test_users|
-  User.create!(
-       email: 'test@test.com',
-    username: 'test',
-    password: 'test',
-    password_confirmation: 'test'
-  )
-  User.create!(
-       email: 'nishant@test.com',
-    username: 'nishant',
-    password: 'nishant',
-    password_confirmation: 'nishant'
-  )
-  User.create!(
-       email: 'morgan@test.com',
-    username: 'morgan',
-    password: 'morgan',
-    password_confirmation: 'morgan'
-  )
-end
-
 
 puts "\n----Finished-----"
