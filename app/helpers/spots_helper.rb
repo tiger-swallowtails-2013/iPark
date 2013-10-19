a87ae3c2b23d93c92193ae8152894c8d7d259560
nathanallen <thenathanator@gmail.com>
2013-10-18 21:57:01 -0700

    stub out user_input search parsing helpers

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -1,2 +1,29 @@
 module SpotsHelper
+  def parse_search(user_input)
+    if user_input =~ /\d{5}-?\d{4}?/
+      #zipcode
+      find_by_zip(user_input)
+    elsif user_input =~ /^([a-zA-Z]+\s?)+/
+      #neighborhood
+      find_by_hood(user_input)
+    elsif user_input =~ /\d+\w?\s\D*/ #skips zip code => \d+\w?\s\D*(\d{5}-?\d{4}?)?/
+      #address
+      find_by_address(user_input)
+    else
+      puts "error parsing search input"
+    end
+  end
+
+  def find_by_zip(input)
+    Spot.where(zip_code: input).limit(10)
+  end
+
+  def find_by_hood(input)
+
+    #checks neighborhood_zipcode_lookup for zipcode
+  end
+
+  def find_by_address(input)
+    #queries google maps for geo-coordinates and/or zipcode
+  end
 end
