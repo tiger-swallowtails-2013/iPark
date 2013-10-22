eeb576c40a941e6ad77cc84bafe2088585e51686
Apprentice <apprentice@dbc-23.local>
2013-10-21 17:06:39 -0700

    add rented_reservations to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :reservations
+  has_many :rented_reservations, class_name: "Reservation", foreign_key: "renter_id"
   has_many :spots, :through => :reservations
   has_many :spots
   validates_presence_of :email, :username, :password_digest, :password_confirmation
