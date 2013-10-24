77606728d43625f2480f2e12a113aca55fb00784
morganwildermuth <morgan.wildermuth@gmail.com>
2013-10-24 01:45:14 -0700

    removed feature session specs since they now work in a completely different fashion

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,18 +1,2 @@
 require 'spec_helper'
 
-feature "user can log in" do
-  let!(:current_user) {FactoryGirl.create(:user)}
-  scenario "when user fills out form to log in correctly should redirect to the spot index page" do
-    login(current_user)
-    expect(page) == spots_path
-  end
-end
-
-feature "user can log out" do
-  let!(:current_user) {FactoryGirl.create(:user)}
-  scenario "when user clicks logout the page should contain 'login'" do
-    login(current_user)
-    logout
-    expect(page).to have_button('Sign In')
-  end
-end
