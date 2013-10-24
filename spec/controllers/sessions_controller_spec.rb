ba04101ee549dcd8bbdc8fc9cb93ec175d97ea04
morganwildermuth <morgan.wildermuth@gmail.com>
2013-10-24 01:44:42 -0700

    added back relevant sessions tests, removed one that was just confirming render rendered

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,1 +1,20 @@
 require 'spec_helper'
+
+describe SessionsController do
+let!(:new_user) { FactoryGirl.create(:user) }
+
+  describe "POST create" do
+    it "creates a new session" do
+      post :create, sessions: {username: new_user.username, password: new_user.password, password_confirmation: new_user.password_confirmation}
+      expect(response).to redirect_to spots_path
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "ends the session" do
+      post :create, sessions: {username: new_user.username, password: new_user.password, password_confirmation: new_user.password_confirmation}
+      delete :destroy 
+      expect(controller.current_user).to be_nil
+    end
+  end
+end
